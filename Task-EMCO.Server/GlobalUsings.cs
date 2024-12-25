862341dac96c10a2d231a65b00f36ff852d2e6ef
AbdulrahmanXSO25 <abdulrahmanxso25@outlook.com>
2024-12-25 16:03:25 +0200

    InitialCommit

---
+++ Task-EMCO.Server/GlobalUsings.cs
@@ -0,0 +1,11 @@
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using System.Text;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.OpenApi.Models;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
